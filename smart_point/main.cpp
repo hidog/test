97a65cdc3c2fdf9e37f87a45399c5b6ca9bc3e2e
hidog <hidog.gao@gmail.com>
2021-04-05 10:07:46 +0800

    add smart point

---
+++ smart_point/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "smart_point.h"
+
+using namespace std;
+
+int main()
+{
+    test_unique_point();
+    
+    
+    return 0;
+}
---
+++ smart_point/test.h
@@ -0,0 +1,19 @@
+#ifndef TEST_H
+#define TEST_H
+
+
+class Test
+{
+public :
+    Test();
+    ~Test();
+    
+    void print();
+    
+private:
+    int *data   =   nullptr;
+
+};
+
+
+#endif // TEST_H
