7b6b93fa2317949b2918c67310dd6587a0a32b24
hidog <hidog@hidogs-iMac.local>
2021-04-13 19:37:44 +0800

    update

--- boost/xcode/xcode/main.cpp
+++ boost/xcode/xcode/main.cpp
@@ -1,33 +1,20 @@
-//
-//  main.cpp
-//  xcode
-//
-//  Created by hidog on 2014/9/24.
-//  Copyright (c) 2014年 hidog. All rights reserved.
-//
 
-#include <iostream>
-#include <boost/thread.hpp>
+#include "../../thread.h"
+#include "../../json.h"
+#include "../../algorithm.h"
+#include "../../uuid.h"
 
-int     f( int thr_id )
-{
-    for( int i = 0; i < 10; i++ )
-        std::cout << "thr = " << thr_id << std::endl;
-    return  10;
-}
 
 int main(int argc, const char * argv[])
 {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    thread_test();
     
+    boost_json_example();
     
-    boost::thread   thr[10];
-    for( int i = 0; i < 10; i++ )
-        thr[i]  =   boost::thread( &f, i );
-    for( int i = 0; i < 10; i++ )
-        thr[i].join();
+    all_of_example();
+    find_not_example();
     
+    generate_uuid_example();
     
     return 0;
 }
