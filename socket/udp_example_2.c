4500c1efb172562cdcb687a82572b9fc8b80ecec
hidog <hidog.gao@gmail.com>
2021-05-04 08:37:26 +0800

    add ip add sleep

--- socket/udp_example_2.c
+++ socket/udp_example_2.c
@@ -228,6 +228,11 @@ void udp_nonblocking_2(void)
                 printf("send to %s, ret = %d\n", ip_list[target], (int)ret );
             }
         }
+#ifdef _WIN32
+        Sleep(1000);
+#else
+        sleep(1);
+#endif
     }
 
 #ifdef _WIN32
