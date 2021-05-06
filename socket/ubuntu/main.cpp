8fd17bd873b66c433c2c9974e371e3b0277ff46c
hidog <hidog.gao@gmail.com>
2021-05-06 14:12:04 +0800

    add ubuntu tcp socket code

--- socket/ubuntu/main.cpp
+++ socket/ubuntu/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "../udp_example.h"
+#include "../tcp_example.h"
 #include "../tool.h"
 
 extern "C"
@@ -33,7 +34,9 @@ int main()
     //udp_nonblocking_2();
     
     //udp_broadcast_recver();
-    udp_broadcast_sender();
+    //udp_broadcast_sender();
+    
+    tcp_hello_server(8888);
 
     return 0;
 }
