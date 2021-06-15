cf2955ecb471d2b78f070de0e0967a970183cb9a
hidog <hidog.gao@gmail.com>
2021-06-15 23:43:01 +0800

    add ffmpeg srt code.

---
+++ SRT_test/ffmpeg_client.h
@@ -0,0 +1,25 @@
+﻿#ifndef FFMPEG_CLIENT_H
+#define FFMPEG_CLIENT_H
+
+#include <string>
+#include <srt.h>
+
+
+
+//#define CLIENT_SEND
+#define CLIENT_RECV
+
+
+
+int ffmpeg_client( std::string ip, std::string port );
+
+
+#ifdef CLIENT_SEND
+void client_send( SOCKET fhandle );
+#elif defined(CLIENT_RECV)
+void client_recv( SRTSOCKET handle );
+#endif
+
+
+
+#endif
--- SRT_test/vs2019/vs2019/vs2019.cpp
+++ SRT_test/vs2019/vs2019/vs2019.cpp
@@ -3,22 +3,19 @@
 #include "srt_client.h"
 #include "srt_server.h"
 
+#include "ffmpeg_server.h"
+#include "ffmpeg_client.h"
+
 
 /*
-    備忘
-    #ifdef SEND_DATA
     _setmode( _fileno(stdin), O_BINARY );
-    #elif defined(RECV_DATA)
     _setmode( _fileno(stdout), O_BINARY );
-    #endif
-    做這個的範例
     讓stdout, stdin可以用binary的方式操作
     可以 ffmpeg ... | srt_test  串接不同的程式
-
 */
 
 
-int main()
+int main( int argc, char* argv[] )
 {
     /*
         測試延遲結果
@@ -28,13 +25,54 @@ int main()
         * 理論上應該要多開一個socket負責回傳,但目的只是簡單測試,就算了
         * 需要 20次round sleep一次, 頻率下降會直接掛掉 
         * 看起來延遲還是無法低於10frame. 
-        * 用ffmpeg, 40M再次測試
         * 執行一段時間後(約一小時吧),程序死當,不知道是否為sleep頻率過低造成. 但sleep過高會造成流量不足
+
+        * 用ffmpeg, 40M再次測試, 確認一下實用性
     */
     //srt_client_test();
     //srt_server_test();
 
-    system("PAUSE");
+    std::string ip, port;
+
+    if( argc == 2 )
+    {
+        port = std::string(argv[1]);
+        ffmpeg_server(port);
+    }
+    else if( argc == 3 )
+    {
+        ip = std::string(argv[1]);
+        port = std::string(argv[2]);
+        ffmpeg_client(ip,port);
+    }
+
     return 0;
 }
+
+
+
+
+/*
+拿來study各種lock的用法
+
+void output_to_stdout()
+{
+    while(true)
+    {
+        if( g_list.empty() == true )
+            Sleep(1);
+        else
+        {
+            std::unique_lock<std::mutex> lck(g_mtx,std::defer_lock);
+            lck.lock();
+            char *ptr = g_list.front();
+            g_list.pop_front();
+            lck.unlock();
+
+            fwrite( ptr, 1316, 1, stdout );
+            delete [] ptr;
+        }
+    }
+}
+*/
 
