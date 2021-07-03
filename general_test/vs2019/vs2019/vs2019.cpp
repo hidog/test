c0639b2d5ebd27b107a0c1c5bfcab5a3320e6ae2
hidog <hidog.gao@gmail.com>
2021-07-03 15:35:55 +0800

    add linklist test code.

--- general_test/vs2019/vs2019/vs2019.cpp
+++ general_test/vs2019/vs2019/vs2019.cpp
@@ -1,20 +1,34 @@
-// vs2019.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "src/linklist.h"
+
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    LinkList	list;
+
+    cout << list.size() << endl;
+    list.print();
+
+    list.push_back(444);
+    list.print();
+
+    list.insert( 0, 3 );
+    list.insert( 0, 32 );
+    list.insert( 1, 35 );
+    for( int i = 0; i < 100; i++ )
+        list.insert( i, i + 55 );
+
+    list.print();
+
+
+    cout << "size = " << list.size() << endl;
+
+    LinkList	list2(list);
+    list2.print();
+
+    system("PAUSE");
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
