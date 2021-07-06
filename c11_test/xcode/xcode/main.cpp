6fe92fcdcea1b36e8b5d6b3b97c9196ee6d4715b
hidog <hidog.gao@gamil.com>
2021-07-06 20:19:47 +0800

    update

--- c11_test/xcode/xcode/main.cpp
+++ c11_test/xcode/xcode/main.cpp
@@ -1,26 +1,14 @@
+//
+//  main.cpp
+//  xcode
+//
+//  Created by hidog on 2021/7/6.
+//
+
 #include <iostream>
 
-#include "c11_sample.h"
-
-
-int main(int argc, const char * argv[]) 
-{
-    std::cout << "This is test.\n";
-
-    c11_sample_1();
-    c11_sample_2();
-    c11_sample_3();
-    c11_sample_4();
-    c11_sample_5();
-    c11_sample_6();
-    c11_sample_7();
-    c11_sample_8();
-    //c11_sample_9();
-    c11_sample_10();
-    c11_sample_11();
-    c11_sample_12();
-    c11_sample_13();
-    c11_swap();
-    
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
     return 0;
 }
