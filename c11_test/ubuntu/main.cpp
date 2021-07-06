37c79ff71515274ece2bb4f759adbce3aa658319
hidog <hidog.gao@gmail.com>
2021-07-06 19:23:42 +0800

    add file

---
+++ c11_test/ubuntu/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include "../c11_sample.h"
+
+int main()
+{
+    std::cout << "This is test.\n";
+
+    c11_sample_1();
+    c11_sample_2();
+    c11_sample_3();
+    c11_sample_4();
+    c11_sample_5();
+    c11_sample_6();
+    c11_sample_7();
+    c11_sample_8();
+    //c11_sample_9();
+    c11_sample_10();
+    c11_sample_11();
+    c11_sample_12();
+    c11_sample_13();
+    c11_swap();
+}
+
