b277d97fc4cc13ab3150872e5279bb45b5000fbd
hidog <hidog.gao@gmail.com>
2021-07-06 19:38:21 +0800

    add linux hardware proj

---
+++ hardware/ubuntu/main.cpp
@@ -0,0 +1,19 @@
+﻿
+#include "CPU.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "get core = " << cpu::get_real_cpu_core() << "\n\n";
+    cout << "get core = " << cpu::get_logical_cpu_core() << "\n\n";
+
+    cout << "vendor = " << cpu::get_cpu_vender() << "\n\n";
+    cout << "cpu id = " << cpu::get_cpu_serial() << "\n\n";
+    cout << "cpu id = " << cpu::cpu_get_serial_asm() << "\n\n";
+
+    system("PAUSE");
+}
+
