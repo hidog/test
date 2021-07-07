4db11d0a7927024d06bac988ed91a400b059488b
hidog <hidog.gao@gmail.com>
2021-07-07 16:16:43 +0800

    update xcode proj

--- general_test/xcode/xcode/main.cpp
+++ general_test/xcode/xcode/main.cpp
@@ -1,14 +1,33 @@
-//
-//  main.cpp
-//  xcode
-//
-//  Created by hidog on 2021/7/7.
-//
-
 #include <iostream>
+#include "linklist.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+
+int main()
+{
+    LinkList    list;
+
+    cout << list.size() << endl;
+    list.print();
+
+    list.push_back(444);
+    list.print();
+
+    list.insert( 0, 3 );
+    list.insert( 0, 32 );
+    list.insert( 1, 35 );
+    for( int i = 0; i < 100; i++ )
+        list.insert( i, i + 55 );
+
+    list.print();
+
+
+    cout << "size = " << list.size() << endl;
+
+    LinkList    list2(list);
+    list2.print();
+
     return 0;
 }
+
