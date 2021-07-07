bbb2e64fc2a99f6a8f950ac0c9be0fc27406f1bf
hidog <hidog.gao@gmail.com>
2021-07-07 16:19:03 +0800

    add xcode proj

---
+++ hardware/xcode/xcode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  xcode
+//
+//  Created by hidog on 2021/7/7.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
