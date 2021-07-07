be39a8dbe5d38c6503ea09133b69a5c21838d888
hidog <hidog.gao@gmail.com>
2021-07-07 16:55:16 +0800

    update

--- STL_test/xcode/xcode/main.cpp
+++ STL_test/xcode/xcode/main.cpp
@@ -1,14 +1,32 @@
-//
-//  main.cpp
-//  xcode
-//
-//  Created by hidog on 2021/7/7.
-//
 
 #include <iostream>
+#include "file.h"
+#include "timer.h"
+#include "smart_point.h"
+#include "test.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    test_unique_point();
+    Test test;
+    test.print();
+
+    std::cout << "file size example : " << get_file_size() << "\n";
+
+    Timer timer;
+    timer.start();
+
+    for( int i = 0; i < 100000; i++ )
+        std::cout << ".";
+    std::cout << "\n";
+
+    timer.end();
+
+    int hour, min, sec, msec, totalms;
+    timer.get_time( hour, min, sec, msec, totalms );
+    std::cout << "timer = " << timer.get_time() << "ms\n";
+    std::cout << hour << " " << min << " " << sec << " " << msec << " " << totalms  << "\n";
+
     return 0;
 }
+
