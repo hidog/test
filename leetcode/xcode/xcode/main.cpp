27f6e908d305073ec6dd688d1597849ad8448734
hidog <hidog.gao@gmail.com>
2022-03-20 09:03:42 +0800

    add xcode proj

---
+++ leetcode/xcode/xcode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  xcode
+//
+//  Created by hidog on 2022/3/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
