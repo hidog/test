6c88fb9100694149382470a24ffe2c977ff30db7
hidog <hidog.gao@gmail.com>
2022-03-20 09:10:19 +0800

    add files

--- leetcode/xcode/xcode/main.cpp
+++ leetcode/xcode/xcode/main.cpp
@@ -1,14 +1,10 @@
-//
-//  main.cpp
-//  xcode
-//
-//  Created by hidog on 2022/3/20.
-//
+#include "p1.h"
+#include "p8.h"
 
-#include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int argc, const char * argv[]) 
+{
+    p1_two_sum();
+
     return 0;
 }
