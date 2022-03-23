af4b96007193c69897f6f95aa8e0ec4b802d195a
hidog <hidog.gao@gmail.com>
2022-03-23 10:44:52 +0800

    update code

---
+++ socket/p2p_example.cpp
@@ -0,0 +1,11 @@
+#include "p2p_example.h"
+
+
+
+void p2p_server_1()
+{}
+
+
+
+void p2p_client_1()
+{}
---
+++ socket/p2p_example.h
@@ -0,0 +1,10 @@
+#ifndef P2P_EXAMPLE_H
+#define P2P_EXAMPLE_H
+
+void p2p_server_1();
+void p2p_client_1();
+
+
+
+
+#endif
