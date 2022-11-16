f4d497bad26e69f13547317c864192eda4e0d070
hidog <hidog.gao@gmail.com>
2022-11-16 19:02:41 +0800

    add code

--- tmp/dictionary.cpp
+++ tmp/dictionary.cpp
@@ -1,20 +1,61 @@
-// dictionary.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <iomanip>
+#include <cassert>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int i, n;
+    int count = 0;
+    int index_1 = 0, index_2 = 0;
+    cin >> n;
+    string str;
+    str.resize(n);
+
+    // init
+    for( i = 0; i < n; i++ )
+        str[i] = 'a' + i;
+
+    // start
+    while(true)
+    {
+        cout << setw(8) << count++ << ": " << str << endl;       
+        //if( count == 2 )
+         //   printf("test");
+
+        for( i = n-2; i >= 0; i-- )
+        {
+            if( str[i+1] > str[i] )
+            {
+                index_1 = i;
+                break;
+            }
+        }
+        if( i < 0 )
+            break;
+
+        for( i = n-1; i > index_1; i-- )
+        {
+            if( str[i] > str[index_1] )
+            {
+                index_2 = i;
+                break;
+            }
+        }
+
+        swap( str[index_1], str[index_2] );
+
+        for( i = index_1+1; i < (index_1+n+1)/2; i++ )
+            swap( str[i], str[n-i+index_1] );
+    }
+
+    int checksum = 1;
+    for( i = 2; i <= n; i++ )
+        checksum *= i;
+    cout << "n! = " << checksum << endl;
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
