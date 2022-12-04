78c8759954bb9586fc2c5f8652859522cdf91878
hidog <hidog.gao@gmail.com>
2022-12-04 19:52:24 +0800

    update

--- boost_test/src/b_process.cpp
+++ boost_test/src/b_process.cpp
@@ -2,17 +2,44 @@
 
 #include <boost/process.hpp>
 #include <iostream>
-#include <cstring>
+#include <boost/filesystem.hpp>
 #include <vector>
+#include <sstream>
+
 
 using namespace boost::process;
+using namespace boost::filesystem;
 
-
-bool    crack_zip()
+bool ScanDirectory( const path& rPath )
 {
-#if 0
+  std::cout << " Directory: " << rPath << std::endl;
+  std::vector<path> vList;
+  copy( directory_iterator(rPath), directory_iterator(), back_inserter( vList ) );
+  /*for( auto itr = vList.begin(); itr != vList.end(); ++itr )
+  {
+    std::cout << "File: " << itr->filename() << "\n";
+  }*/
+
+  if( vList.size() > 0 )
+      return true;
+  else
+      return false;
+}
+
+
+
+bool    unzip( std::string& password )
+{
     ipstream pipe_stream;
-    child c("J:\\UnRAR.exe x -Pabc \"J:\\test.rar\" \"J:\\tmp\"", std_out > pipe_stream);
+    //char str[1000];
+    //sprintf( str, "J:\\UnRAR.exe x -P%s \"J:\\test.rar\" \"J:\\tmp\"", password.c_str() );
+
+    std::ostringstream os;
+    os << "J:\\UnRAR.exe x -inul -p\"" << password << "\" \"J:\\test.rar\" \"J:\\tmp\"";
+    std::cout << os.str() << "\n";
+
+
+    child c( os.str(), std_out > pipe_stream );
 
     //UNRAR E -INUL -P%PASS% "%PATH%\%NAME%" "%TMP%"
 
@@ -22,7 +49,20 @@ bool    crack_zip()
         std::cerr << line << std::endl;
 
     c.wait();
-#endif
+
+    return true;
+}
+
+
+
+
+
+bool    crack_zip()
+{
+    //std::cout << (Scairectory("J:\\test\\tmp") ? "yes" : "no");
+    //return true;
+
+
 
 
     std::string str = "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./ ~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?";
@@ -33,7 +73,7 @@ bool    crack_zip()
     int n = 3;
     int index;
 
-    vec.resize(3);
+    vec.resize(n);
 
     std::string output;
     output.resize(n);
@@ -43,7 +83,12 @@ bool    crack_zip()
     {        
         for( int i = 0; i < vec.size(); i++ )
             output[i] = str[vec[i]];
-        std::cout << output << "\n";
+        //std::cout << output << "\n";
+
+        unzip( output );
+        if( ScanDirectory( "J:\\tmp" ) == true )
+            break;
+
 
         index = 0;
         while(true)
@@ -66,7 +111,7 @@ bool    crack_zip()
     }
     
 
-
+    std::cout << "password = " << output << "\n";
 
     return true;
 }
--- boost_test/src/b_process.h
+++ boost_test/src/b_process.h
@@ -1,9 +1,11 @@
 #ifndef B_PROCESS_H
 #define B_PROCESS_H
 
+#include <string>
+
 
 bool    crack_zip();
-
+bool    unzip( std::string& password );
 
 
 #endif
